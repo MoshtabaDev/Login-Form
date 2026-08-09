a8c09a70420969ebeac1d5d72687cd0365f93e9b
Mojtaba <139409074+MoshtabaDev@users.noreply.github.com>
2026-08-09 05:35:22 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={Navigate to="/sign-in" replace} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/Login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
